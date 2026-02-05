8c24023109cf6c0909b19059ceb1c025b70c32ec
manasamashyal <110389157+manasamashyal@users.noreply.github.com>
2026-02-05 23:31:30 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("addition   :",a+b)
+print("subtraction   :",a-b)
+print("multiplication   :",a*b)
+print("division   :",a/b)
+print("modulus   :",a%b)
+
+
